4f07173518154723c1b4308cf7c6bfce7d3d5eb2
Thibault Zanini <thibaultzanini@gmail.com>
2021-07-01 18:42:23 +0200

    refactor TModel

--- lib/logic/core/model.dart
+++ lib/logic/core/model.dart
@@ -3,7 +3,7 @@ import 'package:equatable/equatable.dart';
 abstract class TModel extends Equatable {
   const TModel();
 
-  dynamic copyWith();
-  dynamic merge();
-  dynamic clone();
+  TModel copyWith();
+  TModel merge(TModel model);
+  TModel clone();
 }
